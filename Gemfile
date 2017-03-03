52b38eeb9461e66d7814fb344076ce4a96ded380
Jose Antonio Guerra <jaguerra@tuenti.com>
2017-03-03 10:07:49 +0100

    Google OAuth2 integration tested, dotenv support

--- Gemfile
+++ Gemfile
@@ -55,5 +55,7 @@ group :development do
   gem 'rb-readline'
 end
 
+gem 'dotenv-rails', groups: [:development, :test]
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   get 'onMatchFinished/:key', to: 'webhook#suso_refresh'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post '/auth/:provider/callback', to: 'sessions#create'
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
 end
