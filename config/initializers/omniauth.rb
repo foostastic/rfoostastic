04e81587cd24490b8fd14d2df7b88ec94103152c
Jose Antonio Guerra <jaguerra@tuenti.com>
2017-04-20 18:35:06 +0200

    Add support for restricting login domains on Google oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
+
+  limit_domains = ENV["GOOGLE_LIMIT_DOMAINS"] || false
+  google_options = {}
+  google_options[:hd] = limit_domains.split(" ") if limit_domains
+  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], google_options
 end
